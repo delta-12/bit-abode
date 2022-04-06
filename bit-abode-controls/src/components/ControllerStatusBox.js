8fb79dff78940d38c8178ce00c243f92b5f84098
delta-12 <btesq235@gmail.com>
2022-04-06 19:59:54 -0400

    FYC

--- bit-abode-controls/src/components/ControllerStatusBox.js
+++ bit-abode-controls/src/components/ControllerStatusBox.js
@@ -34,13 +34,13 @@ export default class ControllerStatusBox extends Component {
     }
     
     render() {
-        const date = new Date(this.props.dateConnected).toLocaleString()
+        // const date = new Date(this.props.dateConnected).toLocaleString()
         return (
             <div className="p-3">
                 <AddDeviceModal show={this.state.showAddDeviceModal} onHide={this.toggleModal} onChange={this.onChange} name={this.state.name} type={this.state.type} port={this.state.port} addDevice={this.addDevice} success={this.state.success} />
                 <div className="d-flex col-12 justify-content-between align-items-center p-3 border-bottom">
                     <h3>{this.props.name}</h3>
-                    <div>
+                    <div className="d-flex">
                         {(this.props.dashboardStatus) ? <button className="btn btn-outline-dark mx-2" onClick={this.toggleModal}>Add Device</button> : null}
                         {(this.props.dashboardStatus) ? <button className="btn btn-primary" onClick={this.props.disconnect}>Disconnect</button> : null}
                     </div>
@@ -49,12 +49,12 @@ export default class ControllerStatusBox extends Component {
                     <div className="col-12">
                         <p>{(this.props.dashboardStatus) ? <span className="text-success">&#10004;</span> : <span className="text-danger">&#10006;</span>}  Dashboard Connected</p>
                         <p>{(this.props.controllerStatus) ? <span className="text-success">&#10004;</span> : <span className="text-danger">&#10006;</span>}  Controller Connected</p>
-                        <p><strong>Local Network (LAN): </strong>{this.props.localNetwork}</p>
+                        {/* <p><strong>Local Network (LAN): </strong>{this.props.localNetwork}</p> */}
                         <p><strong>Local Address: </strong>{this.props.localAddress}</p>
-                        <p><strong>Devices: </strong>{this.props.deviceNum}</p>
+                        {/* <p><strong>Devices: </strong>{this.props.deviceNum}</p>
                         <p><strong>Ports in Use: </strong></p>
                         <p><strong>Available Ports: </strong></p>
-                        <p><strong>Data Connected: </strong>{date}</p>
+                        <p><strong>Data Connected: </strong>{date}</p> */}
                     </div>
                 </div>
             </div>
